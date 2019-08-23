6d078f26233488288cd401de88db795105d9ad18
geekaboo93 <hoye.kong@veltra.com>
2019-08-23 12:02:56 +0800

    enhance judgeCircle

--- source/easy/judgeCircle.php
+++ source/easy/judgeCircle.php
@@ -1,25 +1,32 @@
 <?php
 
-function judgeCircle($moves) {
+function judgeCircle($moves)
+{
     $splited_moves = str_split($moves);
-    $directions = ["U" => 1, "D" => -1 , "L" => -1, "R" => 1];
-    $curr_location = [0,0];
+    $x = 0;
+    $y = 0;
 
-    foreach($splited_moves as $node => $move) {
-        $val = isset($directions[$move]) ? $directions[$move] : 0;
-
-        if($move == "U" || $move == "D") {
-            $curr_location[0] = $curr_location[0] + $val;
-        } else {
-            $curr_location[1] = $curr_location[1] + $val;
+    foreach ($splited_moves as $node => $move) {
+        if ($move == "U") {
+            $y += 1;
+        } else if ($move == "D") {
+            $y += -1;
+        } else if ($move == "L") {
+            $x += -1;
+        } else if ($move == "R") {
+            $x += 1;
         }
     }
 
-    if($curr_location[0] == 0 && $curr_location[1] == 0) {
+    if ($x == 0 && $y == 0) {
         return true;
     }
     return false;
 }
 
 $result = judgeCircle("UDLRLRUULLRLLLLLLL");
-print_r($result."\n");
+print_r($result . "\n");
+$result = judgeCircle("");
+print_r($result . "\n");
+$result = judgeCircle("RLUURDDDLU");
+print_r($result . "\n");
