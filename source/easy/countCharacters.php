62b8d1a54499b46dccc3aa1951b3f49eb4ad4926
geekaboo93 <doub1ea1006@gmail.com>
2019-09-03 17:15:18 +0800

    pending countCharacters

---
+++ source/easy/countCharacters.php
@@ -0,0 +1,13 @@
+<?php
+
+function countCharacters($words, $chars) {
+    $result = 0;
+   foreach($words as $word) {
+       $n = strlen($word);
+       for($i = 0; $i > $n; $i++ ) {
+
+       }
+   }
+}
+$result = countCharacters(["cat","bt","hat","tree"],'atach');
+echo $result."\n";
