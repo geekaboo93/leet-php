41cd057ef0ccde1aad9dd107c78656eb17059080
geekaboo93 <doub1ea1006@gmail.com>
2019-10-02 09:10:33 +0800

    EASY-929-numUniqueEmails

---
+++ source/easy/numUniqueEmails.php
@@ -0,0 +1,25 @@
+<?php
+function numUniqueEmails($emails) {
+    $result = [];
+    if(count($emails) > 100 || count($emails) < 1) {
+        return $result;
+    }
+
+    foreach($emails as $email) {
+        $splited = explode("@",$email);
+        $first = $splited[0];
+        $second = $splited[1];
+
+        $first = str_replace(".","",$first);
+        $temp = substr($first, 0, strpos($first, "+"));
+        $first = !empty($temp) ? $temp : $first;
+        array_push($result, $first."@".$second);
+    }
+    $result = array_unique($result);
+    return count($result);
+}
+
+$result = numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]);
+var_dump($result);
+$result = numUniqueEmails(["testemail@leetcode.com","testemail1@leetcode.com","testemail+david@lee.tcode.com"]);
+var_dump($result);
