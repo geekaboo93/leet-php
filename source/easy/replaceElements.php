e224f496ed52e3907074bcfd2b3571c8fb915b61
geekaboo93 <doub1ea1006@gmail.com>
2020-02-08 01:37:02 +0800

    #1299, replaceElements

---
+++ source/easy/replaceElements.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @param Integer[] $arr
+ * @return Integer[]
+ */
+function replaceElements($arr) {
+    for($i = count($arr)-1, $mx = -1; $i >=0; $i--) {
+        $mx = max($arr[$i], $arr[$i] = $mx);
+    }
+    return $arr;
+}
+$result = replaceElements([17,18,5,4,6,1]);
+print(json_encode($result));
